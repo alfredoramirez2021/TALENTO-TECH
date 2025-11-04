fdb03052876d7636fcc813dd3562f54b5cf81ada
alfredoramirez2021 <78843571+alfredoramirez2021@users.noreply.github.com>
2025-11-04 20:59:52 -0300

    Add files via upload

---
+++ lista_de_productos.py
@@ -0,0 +1,27 @@
+productos = [
+                ["Peine" , "Higiene", 1200],    
+                ["Crema" , "Estética", 7000],
+                ["Jabon de Tocador" , "Higiene", 2000]
+            ]
+
+# print(productos[0])
+# print(productos[1])
+# print(productos[2])
+
+for i in range(0,len(productos),1):
+    print("Nombre del producto: ", productos[i][0])
+    print("Categoria: ", productos[i][1])
+    print("Precio: ", productos[i][2])
+    print("")
+
+# print(productos)
+
+# productos.append(["Cepillo", "Higiene", 3000])
+
+# print(productos)
+
+if "ae" in "aeiou":
+    print ("Encontre")
+
+
+
