b6bbb2addf7fe50e9448509f4eb060fae3f90ea1
alfredoramirez2021 <78843571+alfredoramirez2021@users.noreply.github.com>
2025-11-04 20:51:18 -0300

    EXPRESIONES COMPARATIVAS

---
+++ 2_expresiones_logicas.py
@@ -0,0 +1,19 @@
+# Expresion Lógica, permite unir varias expresiones relacionales o compartivas
+# Operadores: and, or, not
+
+# and (ambas expresiones comparativas debes ser verdaderos para que el resultado sea verdadero)
+
+x = 5
+print(x > 0 and x == 5)  # True, porque 5 es mayor que 0 y menor que 10
+
+# or
+
+# or (si una de las expresiones o ambas son verdaderas el resultado de la expresión lógica será verdadero)
+
+x = 5
+print(x == 0 or x < 3)  # True, porque 5 es mayor que 0 y menor que 10
+
+# not
+x = 5
+print (not x > 7)
+
---
+++ 3_condicional_if.py
@@ -0,0 +1,19 @@
+# Condicional if
+# Es establecer un condición cuyo resultado determine como sigue el código
+
+numero = int(input("Ingrese un número entero: "))
+
+if numero > 0:
+    print("El número es positivo.")
+    print("Hola Positivo!")
+else:  
+    if numero == 0:
+        print("El número es cero")
+    else:         
+        print("El número es negativo")
+        
+        
+        
+        
+        
+ 
