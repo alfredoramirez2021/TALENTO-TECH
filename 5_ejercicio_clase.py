74b4d1be0f159cecf9a04be4865163e997c9fe0e
alfredoramirez2021 <78843571+alfredoramirez2021@users.noreply.github.com>
2025-11-04 20:44:48 -0300

    Add files via upload

---
+++ 5_ejercicio_clase.py
@@ -0,0 +1,12 @@
+nombre = input("Ingresá tu nombre: ")
+apellido = input("Ingresá tu apellido: ")
+edad = int(input("Ingresá tu edad: "))
+email = input("Ingresá tu correo electrónico: ")
+
+# Mostramos los datos de forma organizada
+
+print("\n--- Tarjeta de Presentación ---")
+print("Nombre completo:", nombre, apellido)
+print("Edad:", edad, "años")
+print("Correo electrónico:", email)
+print("-------------------------------")
