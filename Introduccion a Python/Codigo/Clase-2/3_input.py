404c6e1bf3b3c1882523d056ae5f6fb3731d6129
Ramirez Alfredo <78843571+alfredoramirez2021@users.noreply.github.com>
2025-11-20 20:40:55 -0300

    Add files via upload

---
+++ Introduccion a Python/Codigo/Clase-2/3_input.py
@@ -0,0 +1,5 @@
+nombre = input("Cual es su nombre: ")
+
+print("Hola " + nombre)
+
+# print(type(nombre))
---
+++ Introduccion a Python/Codigo/Clase-2/4_numeros.py
@@ -0,0 +1,25 @@
+# Tipos de datos numéricos y expresiones aritméticas
+
+# Operaciones aritméticas (+ , - , * , / , // , ** , %)
+
+numero1 = 15
+numero2 = 6
+
+suma = numero1 + numero2
+resta = numero1 - numero2
+
+mod = numero1 % numero2
+
+potencia = numero1 ** numero2
+
+print("Modulo: ", numero1%numero2)
+
+print("La resta es ", resta)
+
+print(suma)
+
+print("Producto:\tManzanas\nPrecio:\t$1.00")
+
+num = 0
+valido = bool(num)
+print(valido)
