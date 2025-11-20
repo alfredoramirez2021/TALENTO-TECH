d3b9a9f63a11a741898908cf3672a804e482265e
Ramirez Alfredo <78843571+alfredoramirez2021@users.noreply.github.com>
2025-11-20 20:56:29 -0300

    Add files via upload

---
+++ Introduccion a Python/Codigo/Clase-9/1_funciones_sin_parametros.py
@@ -0,0 +1,16 @@
+def saludar():
+    print("Hola que tal")
+    print("Bienvenido a nuestro sistema de Ventas\n")
+    
+
+# for i in range(0, 10,1): 
+#     saludar()
+
+saludar()
+
+
+
+
+
+
+
---
+++ Introduccion a Python/Codigo/Clase-9/2_funciones_con_parametros.py
@@ -0,0 +1,8 @@
+def saludar(nom):
+    print("Hola", nom, "!!")
+
+# nombre = input("Ingrese su nombre: ")
+saludar("Pepe")
+
+saludar("Maria")
+
